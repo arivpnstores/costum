36fe209f4b67c128cdf840dc22fb6c7b21d2669a
PT RAJA SERVER PREMIUM <145305412+arivpnstores@users.noreply.github.com>
2026-02-25 19:33:41 +0000

    Update set-dns.sh

--- set-dns.sh
+++ set-dns.sh
@@ -6,8 +6,7 @@ echo "🔧 Setting & Lock DNS..."
 cat <<EOF > /etc/resolv.conf
 # LOCKED DNS
 nameserver 1.1.1.1
-nameserver 8.8.8.8
-nameserver 9.9.9.9
+nameserver 1.0.0.1
 EOF
 
 # Lock file agar tidak diubah system
